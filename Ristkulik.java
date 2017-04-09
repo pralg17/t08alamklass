ddf78aa24e64976e4d0e02e68825eb777a95bab1
MK <mrxkollo@users.noreply.github.com>
2017-04-09 17:32:22 +0300

    Kolmnurga ja ristküliku klassid

---
+++ Ristkulik.java
@@ -0,0 +1,20 @@
+public class Ristkulik {
+
+    double alus1;
+    double alus2;
+
+    public Ristkulik(double alus1, double alus2) {
+        this.alus1 = alus1;
+        this.alus2 = alus2;
+    }
+
+
+    public double pindala() {
+        return alus1 * alus2;
+    }
+
+    public double umbermoot() {
+        return 2 * (alus1 + alus2);
+    }
+
+}
--- Ruut.java
+++ Ruut.java
@@ -6,6 +6,7 @@ public class Ruut {
         this.kulg = kulg;
     }
 
+
     public double pindala() {
         return kulg * kulg;
     }
