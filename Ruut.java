92f22798b4437c2dc6d60edce353387a505ccf79
MK <mrxkollo@users.noreply.github.com>
2017-04-09 17:25:52 +0300

    Ruudu pindala/umbermoot

---
+++ Ruut.java
@@ -0,0 +1,16 @@
+public class Ruut {
+
+    double kulg;
+
+    public Ruut(double kulg) {
+        this.kulg = kulg;
+    }
+
+    public double pindala() {
+        return kulg * kulg;
+    }
+
+    public double umbermoot() {
+        return 4 * kulg;
+    }
+}
