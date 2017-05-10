b28df23e29c1b75147a8134c0d3e0631492ceeef
siimhuts <siimhuts@greeny.cs.tlu.ee>
2017-05-11 00:13:34 +0300

    Finished homework

---
+++ Ring.java
@@ -0,0 +1,26 @@
+
+public class Ring extends Kujundid{
+
+	double raadius;
+
+	public Ring(double raadius){
+
+		this.raadius = raadius;
+
+	}
+
+
+	@Override
+	public double KujundiPindala(){
+
+		return Math.PI * raadius * raadius;
+
+	}
+
+	@Override
+	public double KujundiUmbermoot(){
+
+		return 2 * Math.PI * raadius;
+
+	}
+}
