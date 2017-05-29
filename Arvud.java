9ceed26dd7a1ccd6c9b08c4bdc94a2912fc54945
Toolsten <Toolsten@users.noreply.github.com>
2017-05-29 23:44:50 +0300

    Kodutöö

---
+++ Arvud.java
@@ -0,0 +1,6 @@
+public class Arvud extends Arvutused {
+    public void erinevus(int x, int y) {
+       int z = x - y;
+       System.out.println("Antud arvude erinevus on "+z);
+    }
+ }
---
+++ Rakendus.java
@@ -0,0 +1,12 @@
+public class Rakendus {
+    public static void main(String args[]) {
+       int a = 100, b = 15;
+       Arvud arvutus = new Arvud();
+       
+       arvutus.erinevus(a, b);
+ 
+       
+       arvutus.liitmine(a, b);
+       arvutus.korrutamine(a, b);
+    }
+ }
