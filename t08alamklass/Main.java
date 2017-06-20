68b2375612965abe82365aa5c02420427447ba6b
JamburTheMan <JamburTheMan@users.noreply.github.com>
2017-06-21 00:39:26 +0300

    Add files via upload

---
+++ t08alamklass/Main.java
@@ -0,0 +1,27 @@
+public class Main{
+	public static void main(String[] args){
+			
+			Pitsa singiPitsa=new SingiPitsa();
+			
+			
+			Pitsa juustuPitsa=new JuustuPitsa();
+			
+			System.out.println("Singipitsa info:");
+			System.out.println(singiPitsa.diameeter);
+			System.out.println(singiPitsa.hind);
+			System.out.println(singiPitsa.kalorsus);
+			System.out.println(" ");
+			System.out.println(" ");
+			
+		
+		
+			System.out.println("Juustupitsa info:");
+			System.out.println(juustuPitsa.diameeter);
+			System.out.println(juustuPitsa.hind);
+			System.out.println(juustuPitsa.kalorsus);
+			
+		
+		
+	}	
+	
+} 
---
+++ t08alamklass/SingiPitsa.java
@@ -0,0 +1,7 @@
+public class SingiPitsa extends Pitsa{
+	
+	public SingiPitsa(){
+		kalorsus=860;
+		hind=9.90;
+	}
+}
